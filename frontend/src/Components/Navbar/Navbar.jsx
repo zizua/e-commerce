acb501fa4d6a50b28c6646af26e5bed9c8a0d438
Zizu <Azizu066@gmail.com>
2025-02-03 00:12:09 +0700

    5. create another useState for navbar menu & change the font to poppins

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../Assets/logo.png'
-import cart_icon from '../Assets/cart_icon.png'
+import React, {useState} from 'react';
+import './Navbar.css';
+import logo from '../Assets/logo.png';
+import cart_icon from '../Assets/cart_icon.png';
 
 const Navbar = () => {
+  // 1. create another use state variable --start
+  const [menu, setMenu] = useState("shop");
+  // 1. create another use state variable --end
   return (
     <div className='navbar'>
       <div className="nav-logo">
@@ -11,10 +14,10 @@ const Navbar = () => {
         <p>SHOP!</p>
       </div>
       <ul className="nav-menu">
-        <li>Shop <hr /></li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("mens")}}>Men{menu==="mens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("womens")}}>Women{menu==="womens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
