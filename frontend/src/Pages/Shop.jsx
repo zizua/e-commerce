50360478bcda4014f68b198ce591818860e92537
Zizu <Azizu066@gmail.com>
2025-02-05 20:02:28 +0700

    9. create popular folder & setup the files

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
 
 const Shop = () => {
   return (
@@ -7,6 +8,9 @@ const Shop = () => {
       {/* 6. create hero folder & files --start */}
       <Hero />
       {/* 6. create hero folder & files --end */}
+      {/* 7. create popular folder & files --start */}
+      <Popular />
+      {/* 7. create popular folder & files --end */}
     </div>
   )
 }
