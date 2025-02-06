5709fab5c03537b4d236aaacce9b85cdc906ecb0
Zizu <Azizu066@gmail.com>
2025-02-07 01:14:33 +0700

    15. configure ShopCategory.jsx file, import banner pages & create ShopCategory.css file

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { use, useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  // 15. get the data from the context --start
+  const {all_product} = useContext(ShopContext);
+  // 15. get the data from the context --end
   return (
-    <div>
-        
+    <div className='shop-category'>
+      <img src={props.banner} alt="" />
     </div>
   )
 }
