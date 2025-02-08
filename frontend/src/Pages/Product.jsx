7f692eea8a79961812b3d50c937df27e8b60e60e
Zizu <Azizu066@gmail.com>
2025-02-09 03:56:13 +0700

    19. create productdisplay folder & files, config breadcrum & productdisplay files (for display component product)

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { ShopContext } from '../Context/ShopContext';
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/Breadcrum/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
@@ -9,9 +10,12 @@ const Product = () => {
   const product = all_product.find((e) => e.id === Number(productId));
   return (
     <div>
-      {/* 15. setup & design the product page --start */}
+      {/* 15. setup & design the product page (product navigation) --start */}
       <Breadcrum product={product}/>
-      {/* 15. setup & design the product page --end */}
+      {/* 15. setup & design the product page (product navigation) --end */}
+      {/* 16. setup & design the product page 2 (product image, title & prices) --start */}
+      <ProductDisplay product={product}/>
+      {/* 16. setup & design the product page 2 ( product image, title & prices) --end */}
     </div>
    )
 };
