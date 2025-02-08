a8b931879138430789a61dbc8e1c6d62168d37c8
Zizu <Azizu066@gmail.com>
2025-02-09 01:34:16 +0700

    18. configure product.jsx & create breadcrum folder and files (for link all products to product page)

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Item.css'
+import {Link} from 'react-router-dom'
 
 const Item = (props) => {
   return (
     <div className='item'>
-        <img src={props.image} alt={props.name} />
+        {/* 16. give link for all product to product page --start */}
+        <Link to={`/product/${props.id}`}><img src={props.image} alt={props.name} /></Link> 
+        {/* 16. give link for all product to product page --end */}
+        {/* <img src={props.image} alt={props.name} /> */}
         <p>{props.name}</p>
         <div className="item-prices">
             <div className="item-price-new">
--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { ShopContext } from '../Context/ShopContext';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrum/Breadcrum';
 
 const Product = () => {
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
   return (
     <div>
-        
+      {/* 15. setup & design the product page --start */}
+      <Breadcrum product={product}/>
+      {/* 15. setup & design the product page --end */}
     </div>
-  )
-}
+   )
+};
 
-export default Product
+export default Product;
