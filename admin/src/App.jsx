621ff2ae04a08b9d50a8e513bd47900b84cb5c32
Zizu <Azizu066@gmail.com>
2025-02-19 17:41:43 +0700

    41. setup, config & create responsive navbar, create sidebar folder & files, setup sidebar (for admin panel)

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import Admin from './Pages/Admin/Admin'
 
 const App = () => {
   return (
-    <div>Admin Panel</div>
-  )
-}
+    <div>
+      <Navbar />
+      <Admin />
+    </div>
+  );
+};
 
-export default App
+export default App;
