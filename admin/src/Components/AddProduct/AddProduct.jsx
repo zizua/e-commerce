7b5f20cf186e6901cee38f91e6c59ec92268dd0c
Zizu <Azizu066@gmail.com>
2025-02-20 22:54:10 +0700

    44. create logic & state for adding product to database (-admin panel)

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -3,36 +3,51 @@ import './AddProduct.css';
 import upload_area from '../../assets/Upload_area.svg';
 
 const AddProduct = () => {
-//Add Logic & State for Displaying Uploaded Image before Add to List Product 
-// 1a --start
-    const [image, setImage] = useState(false);
-
-    const imageHandler = (e) => {
-        setImage(e.target.files[0]);
-    }
-    // 1a --end
+  //Add Logic & State for Displaying Uploaded Image before Add to List Product & Add Product to Database --1start
+  // 1a State for Displaying Uploaded Image before Add to List Product --start
+  const [image, setImage] = useState(false);
+  // 1c State for Adding Product to Database --start
+  const [productDetails, serProductDetails] = useState({
+    name: '',
+    image: '',
+    category: 'women',
+    old_price: '',
+    new_price: '',
+  });
+  const imageHandler = (e) => {
+    setImage(e.target.files[0]);
+  };
+  // 1a State for Displaying Uploaded Image before Add to List Product --end
+  const changeHandler = (e) => {
+    setProductDetails({...productDetail,[e.target.name]:e.target.value})
+  }
+  // 1c State for Adding Product to Database --end
 
   return (
     <div className="add-product">
       <div className="addproduct-itemfield">
         <p>Product Title</p>
-        <input type="text" name="name" placeholder="Type here" />
+        {/* <input type="text" name="name" placeholder="Type here" /> */}
+        <input value={productDetails.name} onChange={changeHandler} type="text" name="name" placeholder="Type here" />
       </div>
       <div className="addproduct-price">
         <div className="addproduct-itemfield">
           <p>Price</p>
-          <input type="text" name="old_price" placeholder="Type here" />
+          {/* <input type="text" name="old_price" placeholder="Type here" /> */}
+          <input value={productDetails.old_price} onChange={changeHandler} type="text" name="old_price" placeholder="Type here" />
         </div>
         <div className="addproduct-itemfield">
           <p>Offer Price</p>
-          <input type="text" name="new_price" placeholder="Type here" />
+          {/* <input type="text" name="new_price" placeholder="Type here" /> */}
+          <input value={productDetails.new_price} onChange={changeHandler} type="text" name="new_price" placeholder="Type here" />
         </div>
       </div>
       <div className="addproduct-itemfield">
         <p>Category</p>
-        <select name="category" className="add-product-selector">
-          <option value="men">Men</option>
+        {/* <select name="category" className="add-product-selector"> */}
+        <select value={productDetails} onChange={changeHandler} name="category" className="add-product-selector">
           <option value="women">Women</option>
+          <option value="men">Men</option>
           <option value="kid">Kid</option>
         </select>
       </div>
@@ -41,7 +56,7 @@ const AddProduct = () => {
           {/* <img src={upload_area} className="addproduct-thumnail-img" alt="" /> */}
           {/* 1b */}
           {/* jika gambar yang dipilih bernilai true, maka akan menampilkan gambar yg diupload. jika bernilai false akan menampilkan icon awal upload_area */}
-          <img src={image?URL.createObjectURL(image):upload_area} className="addproduct-thumnail-img" alt="" />
+          <img src={image ? URL.createObjectURL(image) : upload_area} className="addproduct-thumnail-img" alt="" />
         </label>
         {/* <input type="file" name="image" id="file-input" hidden /> */}
         <input onChange={imageHandler} type="file" name="image" id="file-input" hidden />
