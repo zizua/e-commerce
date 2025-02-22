0fa29a9a0db50efb1bf33a2131aeb9c9b3b44ce1
Zizu <Azizu066@gmail.com>
2025-02-22 22:56:23 +0700

    46. create logic for fetch all product from database & display them to listproduct component (-admin panel)

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ListProduct.css'
+import cross_icon from '../../assets/cross_icon.png'
 
 const ListProduct = () => {
+  {/* Navigate between List Product's Component to Add Product's Component */}
+  const [allProducts, setAllProducts] = useState([]); // 1a State for All Products --start
+  
+  // 1b Fetch All Products from Database --start
+  const fetchInfo = async () => { 
+    await fetch('http://localhost:4000/allproducts')
+    .then((res) => res.json()) 
+    .then((data) => {setAllProducts(data)}); // set response data to allProducts
+  }
+
+  // create useEffect for fetch data
+  useEffect(() => { 
+    fetchInfo(); 
+  },[]) // fetch data when component mounted
+  // 1b Fetch All Products from Database --end
+
   return (
     <div className='list-product'>
-
+      <h1>All Product List</h1>
+      <div className="listproduct-format-main">
+        <p>Product</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+      <div className="listproduct-allproducts">
+        <hr />
+        {allProducts.map((product,index) => {
+          return <div key={index} className="listproduct-format-main listproduct-format">
+            <img src={product.image} alt="" className="listproduct-product-icon" />
+            <p>{product.name}</p>
+            <p>${product.old_price}</p>
+            <p>${product.new_price}</p>
+            <p>{product.category}</p>
+            <img className='listproduct-remove-icon' src={cross_icon} alt="" />
+          </div>
+        })}
+      </div>
     </div>
   )
 }
