f7306a8c0f7477cce95b320eff7e28705257ce52
Zizu <Azizu066@gmail.com>
2025-03-07 05:49:22 +0700

    54. create state and handler for form data (-frontend)

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -6,16 +6,26 @@ const LoginSignup = () => {
 
   // create state for login and signup #2
   const [state, setState] = useState("Login"); // default state is login
+  // create state for form data #4
+  const [formData, setFormData] = useState({
+    username:"",
+    password:"",
+    email:""
+  })
+
+  const changeHandler = (e) => {
+    setFormData({...formData, [e.target.name]:e.target.value})
+  }
 
   // create end point for login and signup #3
   const login = async () => {
-    console.log("Login Function Executed");
-    
+    // console.log("Login Function Executed");
+    console.log("Login Function Executed", formData);
   }
   
   const signup = async () => {
-    console.log("Signup Function Executed");
-
+    // console.log("Signup Function Executed");
+    console.log("Signup Function Executed", formData);
   }
 
   // create login and signup component #1b
@@ -26,9 +36,11 @@ const LoginSignup = () => {
         <h1>{state}</h1>
         <div className="loginsignup-fields">
           {/* <input type="text" placeholder="Your Name" /> */}
-          {state === "Sign Up"? <input type="text" placeholder="Your Name" /> : <></>} 
-          <input type="email" placeholder="Email Address" />
-          <input type="password" placeholder="Password" />
+          {/* {state === "Sign Up"? <input type="text" placeholder="Your Name" /> : <></>}  */}
+          {state === "Sign Up"?<input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder="Your Name" /> : <></>} 
+          {/* <input type="email" placeholder="Email Address" /> */}
+          <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder="Email Address" />
+          <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder="Password" />
         </div>
         {/* <button>Continue</button> */}
         <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
