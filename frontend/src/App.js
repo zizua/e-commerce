b4eaa59cf62a1d1fa9c164726dd167a4e29e260e
Zizu <Azizu066@gmail.com>
2025-03-09 23:09:01 +0700

    66. try to deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,9 @@ import Footer from './Components/Footer/Footer';
 import men_banner from './Components/Assets/banner_mens.png'
 import women_banner from './Components/Assets/banner_women.png'
 import kid_banner from './Components/Assets/banner_kids.png'
+import { disableReactDevTools} from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 function App() {
   return (
